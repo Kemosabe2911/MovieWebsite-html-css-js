4ba9be63409ba0a700861edbbe0d0fcddeba9d17
Kemosabe2911 <stevinprince29@gmail.com>
2020-03-18 15:52:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const a = document.getElementById("srch");
 var home= document.getElementById("home-btn");
+var res= document.getElementById("result");
 document.getElementById("btn").addEventListener("click",search);
 function search(){
     if( a.value === ''){
@@ -15,12 +16,12 @@ function search(){
     }
     else{
         document.body.style.overflow="scroll";
-        a.style.marginTop="-18%";
+        a.style.marginTop="-35vh";
         b=document.getElementById("btn");
-        b.style.marginTop="-18%";
-        var res= document.getElementById("result");
+        b.style.marginTop="-35vh";
+        
         res.innerHTML="<h1>Search Results For: "+a.value+"</h1>";
-        res.style.marginTop="-44%";
+        res.style.marginTop="-85vh";
         home.addEventListener("click",function(){
             a.style.marginTop="0%";
             b.style.marginTop="0%";
@@ -28,5 +29,17 @@ function search(){
             res.style.marginTop="45%";
             document.body.style.overflow="hidden";
         })
+        fetchAPI()
     }
+}
+function fetchAPI(){
+    fetch('http://www.omdbapi.com/?apikey=558775ad&t='+ a.value)
+    /*fetch('http://img.omdbapi.com/?apikey=558775ad&s='+ a.value)*/
+    .then((res) => res.json())
+    .then((data)=> {
+        console.log(data);
+        console.log(data.Poster);
+        
+        
+    })
 }
